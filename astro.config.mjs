d2ca7d30e44ebf4f10426690355f11d39239c54e
Dark <dark@dark.ski>
2025-12-31 16:38:55 -0500

    attempt to fix url lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'guides.milohax.org',
+  site: 'https://guides.milohax.org/',
   base: '/',
   integrations: [
     starlight({
